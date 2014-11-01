db26eacc75943f388508ecba9736778f6b0a29fd
Marios Isaakidis <misaakidis@yahoo.gr>
2014-11-01 17:35:29 +0100

    Added license header; Added jsdoc format documentation; Added lang option for the request; Fixed reqCurrentCity passes callback functions to reqAPI

--- openweathermaplib.js
+++ openweathermaplib.js
@@ -1,10 +1,28 @@
+/*
+ * OpenWeatherMap-Lib JavaScript Library
+ * https://github.com/GeeksHubs/openweathermap-lib
+ *
+ * Copyright 2014 Marios Isaakidis and other contributors
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ *
+ * Not affiliated with openweathermap.org,
+ * distributed without any warranty.
+ */
+
+/**
+ * @file A JS library that makes async requests to openweathermap.org API
+ * @author <a href="mailto:misaakidis@yahoo.gr">Marios Isaakidis</a>
+ */
+
 var WeatherLib = (function () {
 	'use strict';
 
 	var conf = {
 		APIkey: '52977103b38b2cf43df8ecfb577874c4',
 		measSystem: 'metric',
-		reqTimeout: 15000
+		reqTimeout: 15000,
+		lang: 'en'
 	};
 
 	var config = function (newConf) {
@@ -17,14 +35,28 @@ var WeatherLib = (function () {
 		if (newConf.reqTimeout !== undefined) {
 			conf.reqTimeout = newConf.reqTimeout;
 		}
+		if (newConf.lang !== undefined) {
+			conf.lang = newConf.lang;
+		}
 	}
 
+	/**
+	 * Private function that makes the GET request to openweathermap.org servers
+	 * 
+	 * @function WeatherLib#reqApi
+	 * @private
+	 * @param {String} apiPath The API resource to request
+	 * @param {JSON} urlOptions Any options to be included in the query (e.g. city or coordinates)
+	 * @param {Function} [callback] The function to be executed after a successful request
+	 * @param {Function} [onError] The callback function to be executed when a request fails/timeouts
+	 */
 	var reqAPI = function (apiPath, urlOptions, callback, onError) {
 		var urlQueryString = 'http://api.openweathermap.org/data/2.5/' + apiPath + '?';
 		for (var key in urlOptions) {
 			urlQueryString += key + '=' + urlOptions[key] + '&';
 		}
 		urlQueryString += 'units=' + conf.measSystem;
+		urlQueryString += '&lang=' + conf.lang;
 		urlQueryString += '&APPID=' + conf.APIkey;
 
 		var callbackFunction = callback || function(){};
@@ -60,7 +92,7 @@ var WeatherLib = (function () {
 	var reqCurrentCity = function (city, callback, onError) {
 		reqAPI('weather', {
 			q: city
-		});
+		}, callback, onError);
 	}
 
 	var reqCustom = function (urlOptions, callback, onError) {
